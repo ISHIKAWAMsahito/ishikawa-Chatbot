0b71df2205fd809fce4f8f44eeaafb36de8ae4bb
石川雅人 <ishikawamasahito3150@gmail.com>
2025-09-21 12:45:34 +0900

    m

--- main.py
+++ main.py
@@ -882,4 +882,4 @@ async def websocket_endpoint(websocket: WebSocket):
 # --------------------------------------------------------------------------
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
